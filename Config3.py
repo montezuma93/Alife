9b9d37d3b8fcb3d13e80c3c8980ccf33e78f5ce5
montezuma93 <42383428+montezuma93@users.noreply.github.com>
2019-09-12 16:25:32 +0200

    Adjusted amount of runs

--- Config3.py
+++ Config3.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-for i in range(42):
+for i in range(2):
     print("Start Config3 run: ", str(i))
     subprocess.call([sys.executable, './Alife.py', "Config3", "alife.agents.evolution/CognitiveEnvolver/{'ObservationSystem':'SinglePropositionSystem','BeliefRevisionSystem':'ConditionalBeliefRevision', 'WorkingMemorySystem':'WorkingMemoryWithEvidence','DecisionMakingSystem':'HumanLikeDecisionMakingUnderUncertaintySystem', 'MentalMap':'False','ObservationSystem_Args':[], 'BeliefRevisionSystem_Args':['False'], 'WorkingMemorySystem_Args':['RANKING',100], 'DecisionMakingSystem_Args':[0.2, 0.5, 'RANKING', 'False', 'False', 5,  14,14,15,7, 7, 7, 7],'CommunicationSystem_Args':['False', 10, 'RANKING'],'yml_file':'Config3.yml'}"])
     print("End Config3 run: ", str(i))
