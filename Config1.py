dd5add7236906d9f7c30bb64f65ef648c732f612
montezuma93 <42383428+montezuma93@users.noreply.github.com>
2019-09-12 17:00:15 +0200

    Adjusted configs

--- Config1.py
+++ Config1.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 
-for i in range(12):
+for i in range(2):
     print("Start Config1 run: ", str(i))
-    subprocess.call([sys.executable, './Alife.py', "Config1", "alife.agents.evolution/CognitiveEnvolver/{'ObservationSystem':'SinglePropositionSystem','BeliefRevisionSystem':'FormalBeliefRevision', 'WorkingMemorySystem':'WorkingMemoryWithEvidence','DecisionMakingSystem':'HumanLikeDecisionMakingUnderUncertaintySystem', 'MentalMap':'False','ObservationSystem_Args':[], 'BeliefRevisionSystem_Args':['False'], 'WorkingMemorySystem_Args':['EVIDENCE',100], 'DecisionMakingSystem_Args':[0.01, 0.9, 'EVIDENCE', 'False', 'False', 5, 14,14,15,7, 7, 7, 7],'CommunicationSystem_Args':['False', 10, 'EVIDENCE'],'yml_file':'Config1.yml'}"])
+    subprocess.call([sys.executable, './ALife.py', "Config1", "alife.agents.evolution/CognitiveEnvolver/{'ObservationSystem':'SinglePropositionSystem','BeliefRevisionSystem':'FormalBeliefRevision', 'WorkingMemorySystem':'WorkingMemoryWithEvidence','DecisionMakingSystem':'HumanLikeDecisionMakingUnderUncertaintySystem', 'MentalMap':'False','ObservationSystem_Args':[], 'BeliefRevisionSystem_Args':['False'], 'WorkingMemorySystem_Args':['EVIDENCE',100], 'DecisionMakingSystem_Args':[0.01, 0.9, 'EVIDENCE', 'False', 'False', 5, 14,14,15,7, 7, 7, 7],'CommunicationSystem_Args':['False', 10, 'EVIDENCE'],'yml_file':'Config1.yml'}"])
     print("End Config1 run: ", str(i))
