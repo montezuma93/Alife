9f88736a748a14ff324a728c8273464e1fb5322a
montezuma93 <42383428+montezuma93@users.noreply.github.com>
2019-09-12 16:20:30 +0200

    Adjusted configs to run

--- SimulationScript.py
+++ SimulationScript.py
@@ -2,7 +2,7 @@ import sys
 import subprocess
 
 procs = []
-configs = [2]
+configs = [2,3,5,6,8,9]
 for i in configs:
     proc = subprocess.Popen([sys.executable, './Config'+str(i)+'.py'], close_fds=True)
     procs.append(proc)
