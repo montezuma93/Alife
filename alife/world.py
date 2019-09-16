5c860e32da261863668226157cc8ee49a381a6d8
montezuma93 <42383428+montezuma93@users.noreply.github.com>
2019-09-16 14:21:38 +0200

    Add more config, increased to 40000step

--- alife/world.py
+++ alife/world.py
@@ -468,6 +468,8 @@ class World:
                     elif step == 10000:
                         self.create_results(test_run_name, self.creatures, step, True)
                     elif step == 20000:
+                        self.create_results(test_run_name, self.creatures, step, True)
+                    elif step == 40000:
                         self.create_results(test_run_name, self.creatures, step, False)    
                         os.remove("./"+test_run_name +".txt")
                         os.remove("./"+test_run_name +".csv")               
